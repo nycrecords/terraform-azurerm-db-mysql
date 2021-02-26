f52ba9d8b2754a0d68789a4aa527e183d35b5462
Joel Castillo <jocastillo@records.nyc.gov>
2021-02-25 20:08:35 -0500

    Update logs

--- r-logs.tf
+++ r-logs.tf
@@ -1,10 +1,15 @@
+data "azurerm_storage_account" "storage_account" {
+  name                = var.logs_storage_account_name
+  resource_group_name = var.logs_storage_account_resource_group
+}
+
 resource "azurerm_monitor_diagnostic_setting" "log_settings_storage" {
   count = var.enable_logs_to_storage ? 1 : 0
 
   name               = "logs-storage"
   target_resource_id = azurerm_mysql_server.mysql_server.id
 
-  storage_account_id = var.logs_storage_account_id
+  storage_account_id = data.azurerm_storage_account.storage_account.id
 
   log {
     category = "MySqlSlowLogs"
